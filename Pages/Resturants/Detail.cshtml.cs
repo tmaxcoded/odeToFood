5c265c273fae948c493a585f4cde62d2d689b2b5
tmaxcoded <tmaxcoded@outlook.com>
2020-03-24 13:41:49 +0100

    first commit

---
+++ Pages/Resturants/Detail.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFoodNew.Core;
+using OdeToFoodNew.Data;
+
+namespace OdeToFoodNew
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData _iresturant;
+
+        public DetailModel(IRestaurantData iresturant)
+        {
+            _iresturant = iresturant;
+        }
+        public Resturant Resturant { get; set; }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Resturant = new Resturant();
+            Resturant = _iresturant.GetById(restaurantId);
+           
+            if(Resturant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Pages/Resturants/List.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFoodNew.Core;
+using OdeToFoodNew.Data;
+
+namespace OdeToFoodNew
+{
+    public class ListModel : PageModel
+    {
+        
+        private readonly IConfiguration Config;
+        private readonly IRestaurantData _restaurantData;
+
+        public string Message { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
+        public IEnumerable<Resturant> Resturants { get; set; }
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            Config = config;
+            _restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            
+            Message = Config["Message"];
+            Resturants = _restaurantData.GetRestaurantsByName(SearchTerm);
+
+        }
+    }
+}
