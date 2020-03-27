7db5fa2334e6fd4324072f2d30202c5af02bee15
tmaxcoded <tmaxcoded@outlook.com>
2020-03-27 10:47:37 +0100

    changes commited

---
+++ Pages/Resturants/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFoodNew.Core;
+using OdeToFoodNew.Data;
+
+namespace OdeToFoodNew
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData _data;
+        public Resturant _Restaurant { get; set; }
+
+        public EditModel(IRestaurantData data)
+        {
+            _data = data;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+              _Restaurant = _data.GetById(restaurantId);
+
+            if(_Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
